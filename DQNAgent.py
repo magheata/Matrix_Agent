e069ff23206a1a6ced302ce7b6ba48f48c5aa2cb
Miruna Andreea Gheata <miruna.gheata1@estudiant.uib.es>
2020-06-23 16:23:25 +0200

    funciona

--- DQNAgent.py
+++ DQNAgent.py
@@ -12,6 +12,7 @@ import tensorflow as tf
 class DQNAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
+        self.dim = int(np.sqrt(self.state_size))
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
         self.gamma = 0.95  # discount rate
@@ -40,7 +41,7 @@ class DQNAgent:
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
         model = Sequential()
-        model.add(Dense(self.state_size, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(4, input_dim=self.state_size, activation='relu'))
         #model.add(Dense(5, activation='relu'))
         #model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss=self._huber_loss,
@@ -57,27 +58,63 @@ class DQNAgent:
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])  # returns action
+        if state is not None:
+            s = state.reshape((1, self.dim * self.dim))
+            act_values = self.model.predict(s)
+            #return np.argmax(act_values[0])  # returns action
+            return np.argmax(act_values[0])
+        return random.randrange(self.action_size)
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
+        #minibatch = self.memory
+        states, targets_f = [], []
         for state, action, reward, next_state, done in minibatch:
-            reshaped_state = np.reshape(state, self.state_size)
-            target = self.model.predict(reshaped_state, steps=10)
-            if done:
-                target[0][action] = reward
-            else:
-                # a = self.model.predict(next_state)[0]
-                t = self.target_model.predict(next_state)[0]
-                target[0][action] = reward + self.gamma * np.amax(t)
-                # target[0][action] = reward + self.gamma * t[np.argmax(a)]
-            self.model.fit(state, target, epochs=1, verbose=0)
+            if state is not None:
+                s = state.reshape((1, self.dim * self.dim))
+                sp = next_state.reshape((1, self.dim * self.dim))
+                reward_real = reward
+                if not done:
+                    reward_real = (reward_real + self.gamma * np.amax(self.model.predict(sp)))  # reward_learned
+                #print("REPLAY")
+                #print(self.model.predict(s))
+                #reward_model = self.model.predict(s).reshape((self.dim, self.dim, 2))
+                #self.model.summary()
+
+                #reward_model = self.model.predict(s).reshape((self.dim, self.dim))
+                reward_model = self.model.predict(s)
+
+                pos_action = 1
+
+                # pos_action = get_services_position(state)
+                #for row in range(len(reward_real)):
+                #    for col in range(len(reward_real[row])):
+                #        if row in pos_action:
+                #            reward_model[row][col] = reward_real[row][col]
+                #        else:
+                #            reward_model[row][col] = [0., 0.]
+                states.append(s)
+                #targets_f.append(reward_model.reshape(1, self.dim * self.dim * 2))
+                targets_f.append(reward_model)
+
+        states = np.array(states).reshape(-1, self.dim * self.dim)
+        targets_f = np.array(targets_f).reshape(-1, self.action_size)
+        history = self.model.fit(states, targets_f, epochs=1, verbose=0)
+        # Keeping track of loss
+        loss = history.history['loss'][0]
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
+        return loss
 
     def load(self, name):
         self.model.load_weights(name)
 
     def save(self, name):
         self.model.save_weights(name)
+
+    def _predict(self, state):
+        reshaped_state = np.reshape(state, self.state_size)
+        target = self.model.predict(reshaped_state, steps=10)
+        size = self.state_size / 2
+        reshaped_target = np.reshape(target, (size, size))
+        return reshaped_target
--- Main.py
+++ Main.py
@@ -2,20 +2,19 @@ from DQNAgent import DQNAgent
 
 import gym
 
-EPISODES = 5000
+EPISODES = 20
 
 if __name__ == "__main__":
     env = gym.make("env:MatrixEnv-v0")
     state_size = env.observation_space.n
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
-    # agent.load("./save/cartpole-ddqn.h5")
     done = False
     batch_size = 32
-
+    #print(env.s)
     for e in range(EPISODES):
         state = env.reset()
-        for time in range(500):
+        for time in range(20):
             # env.render()
             action = agent.act(state)
             next_state, reward, done = env.step_action(action)
@@ -28,5 +27,6 @@ if __name__ == "__main__":
                 break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
+    #print(state)
         # if e % 10 == 0:
         #     agent.save("./save/cartpole-ddqn.h5")
