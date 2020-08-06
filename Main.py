ba37bdaaa81b31662a5a9755001de4bb58e4fb5b
Miruna Andreea Gheata <miruna.gheata1@estudiant.uib.es>
2020-08-06 11:34:53 +0200

    Creada nueva rama

--- Main.py
+++ Main.py
@@ -73,5 +73,5 @@ if __name__ == "__main__":
 
     controller = Controller(int(episodes), int(iterations), int(experiment_type))
     controller.run_experiment()
-    # predictActions(env, agent)
-
+    #predictActions(env, agent)
+    #
